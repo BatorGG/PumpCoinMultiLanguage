6ab7b03cdc912c0a3d30138599db04d722ae084e
BatorGG <bator.karpathegyi@gmail.com>
2018-03-04 19:55:36 +0100

    Update countdownhu.js

--- js/countdownhu.js
+++ js/countdownhu.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Coin: ZSE - Exchange: Cryptopia";
+    document.getElementById("demo").innerHTML = "Coin: CPN - Exchange: Cryptopia";
   }
 }, 1000);
