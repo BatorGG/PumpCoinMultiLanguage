12c843c799f53448495bc16f5bd79971e1082eb9
BatorGG <bator.karpathegyi@gmail.com>
2018-03-07 19:23:16 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date(Date.UTC(2018, 7, 4, 18, 20, 0)).getTime();
+var countDownDate = new Date(Date.UTC(2018, 2, 11, 19, 0, 0)).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Coin: CPN<br>Exchange: Cryptopia";
+    document.getElementById("demo").innerHTML = "Coin: LIT<br> <br>Exchange: Cryptopia";
   }
 }, 1000);
